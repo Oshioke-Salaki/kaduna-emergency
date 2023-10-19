f9c0227cee53f7c51ee554f78d8faba3f2a69485
Oshioke-Salaki <salaki1902@gmail.com>
2023-10-19 01:36:10 -0600

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,9 @@ export default {
         secondaryColor: "#981806",
         greyLight: "#F9F6F6",
       },
+      boxShadow: {
+        shadowPri: "0 0.4rem 1.2rem rgba(36, 42, 46, 0.16)",
+      },
     },
   },
   plugins: [],
