4b7df1d120c2ac7850742344f661fd32dec43afc
Oshioke-Salaki <salaki1902@gmail.com>
2023-10-19 01:27:05 -0600

    update

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Home from "./pages/Home";
 
 function App() {
   return (
-    <div className="max-w-screen h-fit w-screen overflow-x-hidden">
+    <div className="max-w-screen h-fit w-screen overflow-x-hidden pb-[42px]">
       <Home />
     </div>
   );
