a6488e8793b059b32dfb18fdf07007586901d874
Oshioke-Salaki <salaki1902@gmail.com>
2023-10-19 01:05:50 -0600

    update

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -8,7 +8,7 @@ function Map() {
     >
       <MapContainer
         center={[10.3764, 7.7095]}
-        zoom={13}
+        zoom={6}
         scrollWheelZoom={false}
         height="100%"
         width=" 100%"
@@ -16,7 +16,7 @@ function Map() {
       >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
         />
         {/* <Marker position={[51.505, -0.09]}>
         <Popup>
