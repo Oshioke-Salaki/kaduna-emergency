49e4d81cc09dbb0032d6904ecf23f7a5ae113d35
Oshioke-Salaki <salaki1902@gmail.com>
2023-10-18 23:15:32 -0600

    update

--- src/components/RiverLevelBox.jsx
+++ src/components/RiverLevelBox.jsx
@@ -3,15 +3,28 @@ import { riverLevels } from "../data/data";
 function RiverLevelBox() {
   return (
     <div className="w-full">
-      <div className="grid grid-cols-[1fr_1fr_1fr_1fr_1fr]">
-        {riverLevels.map((level, i) => (
-          <div
-            key={i}
-            className={`bg-${level.color} flex items-center justify-center px-[13px] py-[7px] text-center text-[8px] font-semibold text-white`}
-          >
-            {level.level}
-          </div>
-        ))}
+      <div className="border-b-solid w-full border-b-[0.5px] border-b-[#c4c4c4] pb-[9px]">
+        <div className="grid grid-cols-[1fr_1fr_1fr_1fr_1fr]">
+          {riverLevels.map((level, i) => (
+            <div
+              key={i}
+              className={`bg-${level.color} flex items-center justify-center px-[13px] py-[7px] text-center text-[8px] font-semibold text-white`}
+            >
+              {level.level}
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="flex">
+        <h4 className="pb-[10px] pl-[10px] pr-[13px] pt-1">4.78</h4>
+        <div className="h-full w-[0.5px] bg-[#c4c4c4]"></div>
+        <h4 className="pb-[10px] pl-[10px] pr-[13px] pt-1">4.78</h4>
+        <div className="h-full w-[0.5px] bg-[#c4c4c4]"></div>
+        <h4 className="pb-[10px] pl-[10px] pr-[13px] pt-1">4.78</h4>
+        <div className="h-full w-[0.5px] bg-[#c4c4c4]"></div>
+        <h4 className="pb-[10px] pl-[10px] pr-[13px] pt-1">4.78</h4>
+        <div className="h-full w-[0.5px] bg-[#c4c4c4]"></div>
+        <h4 className="pb-[10px] pl-[10px] pr-[13px] pt-1">4.78</h4>
       </div>
     </div>
   );
--- src/data/data.js
+++ src/data/data.js
@@ -2,21 +2,26 @@ export const riverLevels = [
   {
     level: "Very Low",
     color: "#c4c4c4",
+    value: "4.78",
   },
   {
     level: "Low",
     color: "#2ABE11",
+    value: "5.00",
   },
   {
     level: "Normal",
     color: "#FFFE54",
+    value: "4.78",
   },
   {
     level: "Warning",
     color: "#BE6411",
+    value: "8.53",
   },
   {
     level: "Danger",
     color: "#D72020",
+    value: "9.14",
   },
 ];
