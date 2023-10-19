a34b349381c6c4c4a10cd9ca97dbf2e3a937ab92
Oshioke-Salaki <salaki1902@gmail.com>
2023-10-19 00:26:21 -0600

    update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,9 +5,9 @@ import SideControl from "../components/SideControl";
 
 function Home() {
   return (
-    <div>
+    <div className="w-[100vw] max-w-[100vw] overflow-hidden">
       <Navbar />
-      <main className="mb-6 flex gap-x-[53px] bg-white px-8 py-[70px]">
+      <main className="mb-6 flex h-[512px] gap-x-[53px] bg-white px-8 py-[70px]">
         <SideControl />
         <div className="w-full">
           <Map />
