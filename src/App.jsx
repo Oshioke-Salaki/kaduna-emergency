5951702a6ae399d752f8562105c59e33c1d90cb2
Oshioke-Salaki <salaki1902@gmail.com>
2023-10-19 00:35:01 -0600

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import Home from "./pages/Home";
 
 function App() {
-  return <Home />;
+  return (
+    <div className="w-[100vw] max-w-[100vw] overflow-x-hidden">
+      <Home />
+    </div>
+  );
 }
 
 export default App;
