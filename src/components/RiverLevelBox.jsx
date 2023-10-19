646bec06625ac78d9215100bed5e0178097e4a78
Oshioke-Salaki <salaki1902@gmail.com>
2023-10-19 15:49:12 -0600

    update

--- src/components/RiverLevelBox.jsx
+++ src/components/RiverLevelBox.jsx
@@ -1,10 +1,10 @@
 import { riverLevels } from "../data/data";
-import value from '../assets/value.png'
+import value from "../assets/value.png";
 
 function RiverLevelBox() {
   return (
     <div className="w-full">
-      <div className="border-b-solid w-full border-b-[0.5px] border-b-[#c4c4c4] pb-[9px]">
+      <div className="pb-[9px]">
         <div className="grid grid-cols-[1fr_1fr_1fr_1fr_1fr]">
           {riverLevels.map((level, i) => (
             <div
