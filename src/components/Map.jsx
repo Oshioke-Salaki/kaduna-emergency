8c1661e328440568deeee23735727ae4fe2ca0d4
Oshioke-Salaki <salaki1902@gmail.com>
2023-10-19 13:01:57 -0600

    update

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -40,7 +40,7 @@ function Map() {
       </div>
       <MapContainer
         center={mapPosition}
-        zoom={12}
+        zoom={15}
         scrollWheelZoom={false}
         height="100%"
         width=" 100%"
