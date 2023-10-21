f5349150e6867d822c3e282811cc7e242c6f9c90
manoahLinks <manoahluka@gmail.com>
2023-10-21 08:46:28 +0100

    chore: integration setup

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,10 +6,10 @@ import useFetch from "../hooks/useFetch";
 
 function Home() {
   const {data} = useFetch(`https://api.thingspeak.com/channels/2313632/feeds.json?api_key=0PMLQ7B0B9BOGV52`)
-  console.log(data)
+  data && console.log(data.feeds)
   return (
     <>
-      <Navbar />
+     <Navbar />
       <main className="mb-6 flex h-fit w-full gap-x-[53px] bg-white px-8 py-[30px]">
          <SideControl />
 
@@ -17,6 +17,7 @@ function Home() {
       </main>
       <div className="px-8">
         <Danger />
+        {data && <h4>{ JSON.parse(data.feeds?.[data.feeds.length -1].field1 )}</h4>}
       </div>
     </>
   );
